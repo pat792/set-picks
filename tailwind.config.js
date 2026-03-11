d487fb94a88293e2e49dc65c1ef1e84ce897043e
pat792 <pat@road2media.com>
2026-03-10 20:54:16 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,12 @@
 export default {
   content: [
     "./index.html",
-    "./*.{js,ts,jsx,tsx}", 
+    "./*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
