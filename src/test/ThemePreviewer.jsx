6af896db830a28d1a982a1a947d5019a4f7d0b58
pat792 <pat@road2media.com>
2026-03-20 16:11:21 -0600

    themepreview react import

--- src/test/ThemePreviewer.jsx
+++ src/test/ThemePreviewer.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function ThemePreviewer() {
   return (
     <div className="flex flex-col md:flex-row gap-6 p-8 bg-black min-h-screen items-center justify-center">
